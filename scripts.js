6aec64582fa25dac6bfffaed883ad1b7eb6025f8
theInternauts <cricketw@wearetheinternauts.com>
2020-10-18 03:03:55 +0900

    do not need jQuery

--- scripts.js
+++ scripts.js
@@ -1,23 +1,39 @@
-$(document).ready(function() {
-  //option A
-  // I know, I know, I don't need JQuery!
-  $("#dynamicForm").submit(function(evt){
-    evt.preventDefault(evt);
-    window.evt = evt;
-    var snippet = "ENG_" + $(evt.currentTarget).find("input[name='number']").val();
-    var snippet_scene = "_" + $(evt.currentTarget).find("select#example").val();
-    var url = "https://dkjplpdstudyjp.akamaized.net/" + snippet + snippet_scene + ".mp4";
-    console.log("--- ", url);
-    $("#dynamicVideo").attr("src", url);
-    var video = document.getElementById("dynamicVideo");
-    // video.defaultMuted = "true";
-    video.controls = "true";
-    video.seeking = "true";
-    video.loop = "false";
-    video.play();
-  });
+ready(fn);
 
-  $(".control-links a").on('click', function(evt){
-    evt.preventDefault(evt);
-  });
-});
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function fn() {
+  const form = document.getElementById('dynamicForm');
+  form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  window.evt = evt;
+  let url = createSrcURL(evt);
+  setVideoSrc(url);
+}
+
+function createSrcURL(evt){
+  var form = evt.srcElement;
+  var snippet = "ENG_" + form.querySelectorAll("input[name='number']")[0].value;
+  var snippet_scene = "_" + form.querySelectorAll("select#example")[0].value;
+  var url = "https://dkjplpdstudyjp.akamaized.net/" + snippet + snippet_scene + ".mp4";
+  return url;
+}
+
+function setVideoSrc(src){
+  var video = document.getElementById("dynamicVideo");
+  // video.defaultMuted = "true";
+  video.src = src;
+  video.controls = "true";
+  video.seeking = "true";
+  video.loop = "false";
+  video.play();
+}
